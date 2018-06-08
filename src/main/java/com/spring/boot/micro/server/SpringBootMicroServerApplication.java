59ac7a56aefb9a7c4eb6ad4076e12eaf9016f5b8
kotireddy <kotireddyare@gmail.com>
2018-06-08 13:05:26 +0530

    Spring Boot Micro Service Registry Done....

--- src/main/java/com/spring/boot/micro/server/SpringBootMicroServerApplication.java
+++ src/main/java/com/spring/boot/micro/server/SpringBootMicroServerApplication.java
@@ -2,11 +2,14 @@ package com.spring.boot.micro.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringbootMicroServerApplication {
+public class SpringBootMicroServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMicroServerApplication.class, args);
+
+		SpringApplication.run(SpringBootMicroServerApplication.class, args);
 	}
 }
