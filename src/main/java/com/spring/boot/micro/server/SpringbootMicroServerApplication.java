a9d699732aa90f0d218669805c156c13ffbc200a
kotireddy <kotireddyare@gmail.com>
2018-06-08 11:44:22 +0530

    Initial commit

---
+++ src/main/java/com/spring/boot/micro/server/SpringbootMicroServerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.boot.micro.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroServerApplication.class, args);
+	}
+}
